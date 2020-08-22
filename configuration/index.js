ebf7711c9cbcf8e6822aa67be27e43f9bb0c4cf6
fourtyone11 <asleeppiano@outlook.com>
2020-08-22 18:09:12 +0300

    removed scope

--- configuration/index.js
+++ configuration/index.js
@@ -1,6 +1,5 @@
 export const params = {
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
-  redirect_uri: process.env.REDIRECT_URI,
-  scope: 'public_repos, user'
+  redirect_uri: process.env.REDIRECT_URI
 }
